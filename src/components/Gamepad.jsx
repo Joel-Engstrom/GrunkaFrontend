2ee75c7f6994151ec8088f2acbdd68bd999163b3
Joel Engström <joel.engstrom00@gmail.com>
2023-01-21 18:54:50 +0100

    Virtual joystick and response working.

--- src/components/Gamepad.jsx
+++ src/components/Gamepad.jsx
@@ -1,23 +1,32 @@
-import { Box, Snackbar, Alert } from "@mui/material";
+import {
+  Box,
+  Snackbar,
+  Alert,
+  Paper,
+  breadcrumbsClasses,
+  Container,
+  Stack,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
 import "joypad.js"; // ES6
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import Joystick from "./Joystick";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#1A2027",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
 const Gamepad = ({}) => {
   const socket = io("ws://localhost:8000");
-  const [controller, setController] = useState("Ingen kontroller hittades!");
+  const [controller, setController] = useState("Ingen kontroller ansluten!");
   const [state, setState] = useState({ base: 90, lowerArm: 90 });
-  const [leftStick, setLeftStick] = useState({
-    type: "left",
-    horizontal: 0,
-    vertical: 0,
-  });
-  const [rightStick, setRightStick] = useState({
-    type: "right",
-    horizontal: 0,
-    vertical: 0,
-  });
+  const [axes, setAxes] = useState([0, 0, 0, 0]);
   const [open, setOpen] = useState(false);
 
   const handleClick = () => {
@@ -54,6 +63,12 @@ const Gamepad = ({}) => {
   }, []);
 
   useEffect(() => {
+    joypad.on("disconnect", (e) => {
+      setController("Ingen kontroller ansluten!");
+    });
+  }, []);
+
+  useEffect(() => {
     joypad.on("button_press", (e) => {
       const { buttonName } = e.detail;
 
@@ -67,6 +82,7 @@ const Gamepad = ({}) => {
       const { axis, axisMovementValue, stickMoved } = e.detail;
 
       sendToSocket({ stick: stickMoved, axis: axis, value: axisMovementValue });
+      setAxes(e.detail.gamepad.axes);
     });
   }, []);
 
@@ -74,15 +90,41 @@ const Gamepad = ({}) => {
     socket.emit("input", data);
   };
 
-  socket.on("state", (currentState) => {
-    setState(currentState);
-  });
+  useEffect(() => {
+    socket.on("state", (currentState) => {
+      console.log(
+        `Base: ${currentState.base} | Lower arm: ${currentState.lowerArm}`
+      );
+      setState(currentState);
+    });
+  }, []);
 
   return (
     <>
       <Box>{`Använder kontroller: ${controller}`}</Box>
       <Box>{`Basen: [${state.base}]`}</Box>
       <Box>{`Lägre arm: [${state.lowerArm}]`}</Box>
+      <Container>
+        <Stack direction="row" justifyContent="space-between">
+          <Joystick x={axes[0]} y={axes[1]} title="Vänster Joystick" />
+          <Joystick x={axes[2]} y={axes[3]} title="Höger Joystick" />
+        </Stack>
+      </Container>
+
+      <Grid2 container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid2 xs={6}>
+          <Item>1</Item>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Item>2</Item>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Item>3</Item>
+        </Grid2>
+        <Grid2 xs={6}>
+          <Item>4</Item>
+        </Grid2>
+      </Grid2>
 
       {controller !== "Ingen kontroller hittades!" && (
         <Snackbar
@@ -93,6 +135,18 @@ const Gamepad = ({}) => {
         >
           <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
             Använder kontroller: {controller}
+          </Alert>
+        </Snackbar>
+      )}
+      {controller === "Ingen kontroller ansluten!" && (
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        >
+          <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+            Kontroller bortkopplad!
           </Alert>
         </Snackbar>
       )}
--- src/components/Joystick.jsx
+++ src/components/Joystick.jsx
@@ -1,27 +1,47 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-const Joystick = ({ joystickData }) => {
+const Joystick = ({ x, y, title }) => {
   return (
     <Box
-      width={"100px"}
-      height={"100px"}
       sx={{
-        borderRadius: "100%",
-        backgroundColor: "primary.dark",
-        justifyContent: "center",
-        alignItems: "center",
         display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
+      <Typography
+        component="h1"
+        variant="h6"
+        align="center"
+        color="info.main"
+        fontWeight={"bold"}
+      >
+        {title}
+      </Typography>
       <Box
-        width={"50px"}
-        height={"50px"}
+        width={100}
+        height={100}
         sx={{
           borderRadius: "100%",
-          backgroundColor: "secondary.dark",
-          position: "relative",
+          backgroundColor: "darkcyan",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      ></Box>
+      >
+        <Box
+          width={20}
+          height={20}
+          sx={{
+            borderRadius: "100%",
+            backgroundColor: "primary.dark",
+            position: "absolute",
+            transform: `translateX(${x * 40}px) translateY(${y * 40}px)`,
+            transition: "transform 0.1s ease-in-out",
+          }}
+        ></Box>
+      </Box>
     </Box>
   );
 };
